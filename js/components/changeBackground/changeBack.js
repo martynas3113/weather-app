b613f1858beb908e65f55b7d028f2e9c69499b2b
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-07 16:32:37 +0200

    Update changeBack.js

--- js/components/changeBackground/changeBack.js
+++ js/components/changeBackground/changeBack.js
@@ -1,16 +1,16 @@
 function changeBack(){
     const currentTime = new Date().getHours();
     if(currentTime < 6) {
-        document.querySelector('body').style.backgroundImage = "url(../img/night.png)";
+        document.querySelector('body').style.backgroundImage = "url(../img/Night.png)";
     }
     if(currentTime > 6 && currentTime < 12) {
         document.querySelector('body').style.backgroundImage = "url(../img/Morning.png)";
     }
     if(currentTime > 12 && currentTime < 18) {
-        document.querySelector('body').style.backgroundImage = "url(../img/dayTime.png)";
+        document.querySelector('body').style.backgroundImage = "url(../img/DayTime.png)";
     }
     if(currentTime > 18) {
-        document.querySelector('body').style.backgroundImage = "url(../img/evening.png)";
+        document.querySelector('body').style.backgroundImage = "url(../img/Evening.png)";
     }
 }
 
